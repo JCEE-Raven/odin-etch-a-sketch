11c6e09f6166ce139a92cd6bfb34c7b8832d3725
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-07-12 12:37:21 +0100

    ADD create grid pieces
    added variables to grab game container element and to create grid piece
    div also have an initial grid size
    Working on looping through and creating div elements for each grid piece
    using initial grid size

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,2 +1,19 @@
 // Etch a sketch game built for odin foundation course
 
+//variables
+const etchBoardContainer = document.querySelector('.etchBoardContainer');
+const etchGridPiece = document.createElement('div');
+etchGridPiece.classList.add('etchGridPiece', 'blank')
+
+const initialGridSize = 16 * 16;
+console.log("Initial size is " + initialGridSize)
+buildGrid(initialGridSize);
+
+function buildGrid(gridSize){
+    for(let i = 0; i < gridSize; i++){
+        console.log('loop : ' + i);
+        etchBoardContainer.appendChild(etchGridPiece);
+    }
+
+}
+
