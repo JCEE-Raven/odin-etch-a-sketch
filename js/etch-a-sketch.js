e26ca99157b0711de88d3d174765dff46deed322
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-07-12 13:45:52 +0100

    Add function to make grid
    function to make grid populates board container with grid items that
    auto size to fill based on a square grid

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -2,16 +2,20 @@
 
 //variables
 const etchBoardContainer = document.querySelector('.etchBoardContainer');
-const etchGridPiece = document.createElement('div');
-etchGridPiece.classList.add('etchGridPiece', 'blank')
 
-const initialGridSize = 16 * 16;
-console.log("Initial size is " + initialGridSize)
+//create initial grid
+const initialGridSize = 16;
+console.log("Initial size is " + initialGridSize * initialGridSize)
 buildGrid(initialGridSize);
 
-function buildGrid(gridSize){
+function buildGrid(halfGridSize){
+    let gridSize = halfGridSize * halfGridSize;
     for(let i = 0; i < gridSize; i++){
         console.log('loop : ' + i);
+        const etchGridPiece = document.createElement('div');
+        etchGridPiece.classList.add('etchGridPiece', 'blank')
+        etchGridPiece.style.minHeight = "calc(100% / " + halfGridSize  + ")";
+        etchGridPiece.style.minWidth = "calc(100% / " + halfGridSize  + ")";
         etchBoardContainer.appendChild(etchGridPiece);
     }
 
